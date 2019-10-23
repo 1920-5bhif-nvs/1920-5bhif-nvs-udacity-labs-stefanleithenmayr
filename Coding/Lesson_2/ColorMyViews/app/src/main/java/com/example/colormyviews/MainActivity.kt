315958a38665e8e1f92854a29f668d62cd88a4b2
stefanleithenmayr <s.leithenmayr@gmx.at>
2019-10-23 21:54:17 +0200

    2.25 - Adding Box Two Below Box One

--- Coding/Lesson_2/ColorMyViews/app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ Coding/Lesson_2/ColorMyViews/app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.colormyviews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.colormyviews.R.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(layout.activity_main)
     }
 }
